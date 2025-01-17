ec680540ff76f3a6da1df97346dc0d4d89c7c3f9
hejun <hejun@wsl.com>
2025-01-17 12:30:06 +0800

    init rst

---
+++ brst/target/debug/deps/brst-21086888ad5dec75.d
@@ -0,0 +1,5 @@
+/home/compiler_env/projets/rst/pre_rst/brst/target/debug/deps/libbrst-21086888ad5dec75.rmeta: src/main.rs
+
+/home/compiler_env/projets/rst/pre_rst/brst/target/debug/deps/brst-21086888ad5dec75.d: src/main.rs
+
+src/main.rs:
---
+++ brst/target/debug/deps/brst-9dc8d910ada1f27e.d
@@ -0,0 +1,5 @@
+/home/compiler_env/projets/rst/pre_rst/brst/target/debug/deps/libbrst-9dc8d910ada1f27e.rmeta: src/main.rs
+
+/home/compiler_env/projets/rst/pre_rst/brst/target/debug/deps/brst-9dc8d910ada1f27e.d: src/main.rs
+
+src/main.rs:
